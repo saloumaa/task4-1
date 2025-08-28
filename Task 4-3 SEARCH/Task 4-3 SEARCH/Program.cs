8d0d7d943d06dff1a1c76639fbe4ed10f7590a98
salma mohamed <salmamahmed091@gmail.com>
2025-08-29 00:49:47 +0300

    1

--- Task 4-3 SEARCH/Task 4-3 SEARCH/Program.cs
+++ Task 4-3 SEARCH/Task 4-3 SEARCH/Program.cs
@@ -2,9 +2,32 @@
 {
     internal class Program
     {
+        //1.Write a C# program that reads a list of integers from the user and throws an exception if any numbers are duplicates.
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            try
+            {
+                Console.Write("Enter numbers: ");
+                string[] parts = Console.ReadLine().Split(' ');
+                List<int> numbers = new List<int>();
+
+                foreach (var p in parts)
+                {
+                    int n = Convert.ToInt32(p);
+                    if (numbers.Contains(n))
+                        throw new Exception("Duplicate found: " + n);
+
+                    numbers.Add(n);
+                }
+
+                Console.WriteLine("All numbers are unique.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
+
+
     }
 }
